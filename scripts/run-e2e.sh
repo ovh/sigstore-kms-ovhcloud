322629746a942b35372bd760fde05c326ee8b309
Kevin Demy <kevin.demy@ovhcloud.com>
2026-06-16 11:06:47 +0200

    test: update tests (unit and integration) to create key with key instead of name
    Signed-off-by: Kevin Demy <kevin.demy@ovhcloud.com>

--- scripts/run-e2e.sh
+++ scripts/run-e2e.sh
@@ -12,14 +12,14 @@ trap cleanup EXIT
 echo "secret message" > test.txt
 
 echo "[1/3] Getting public key..."
-if ! cosign public-key --key ovhcloud://"$KMS_INTEGRATION_KEY_ID" > public.key; then
+if ! cosign public-key --key ovhcloud://"$KMS_INTEGRATION_KEYID" > public.key; then
   echo "TEST FAILED: Failed to retrieve public key from KMS."
   exit 1
 fi
 
 
 echo "[2/3] Signing..."
-if ! cosign sign-blob --key ovhcloud://"$KMS_INTEGRATION_KEY_ID" --bundle test.bundle test.txt; then
+if ! cosign sign-blob --key ovhcloud://"$KMS_INTEGRATION_KEYID" --bundle test.bundle test.txt; then
   echo "TEST FAILED: Failed to sign with KMS key."
   exit 1
 fi
@@ -31,13 +31,13 @@ if ! cosign verify-blob --key public.key --bundle test.bundle test.txt; then
     exit 1
 fi
 
-if ! cosign verify-blob --key ovhcloud://"$KMS_INTEGRATION_KEY_ID" --bundle test.bundle test.txt; then
+if ! cosign verify-blob --key ovhcloud://"$KMS_INTEGRATION_KEYID" --bundle test.bundle test.txt; then
     echo "TEST FAILED: Verification with KMS key failed."
     exit 1
 fi
 
 echo "bad content" > bad.txt
-if cosign verify-blob --key ovhcloud://"$KMS_INTEGRATION_KEY_ID" --bundle test.bundle bad.txt; then
+if cosign verify-blob --key ovhcloud://"$KMS_INTEGRATION_KEYID" --bundle test.bundle bad.txt; then
     echo "TEST FAILED: Verification of bad file should have failed but succeeded!"
     exit 1
 fi
